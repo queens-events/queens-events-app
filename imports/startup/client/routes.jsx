ec7f71d2ac7f5f67fe4156250ff5ecdbc3a92b82
PfeifferH <15hp19@queensu.ca>
2018-07-02 17:30:54 -0400

    Implemented iron router

---
+++ imports/startup/client/routes.jsx
@@ -0,0 +1,20 @@
+// QLICKER
+// Author: Enoch T <me@enocht.am>
+//
+// routes.jsx: iron-router routes
+
+import React from 'react'
+import { mount } from 'react-mounter'
+
+// Layouts and Pages
+import { AppLayout } from '../../ui/layouts/app_layout'
+
+Router.configure({
+  loadingTemplate: 'loading'
+})
+
+Router.route('/', function () {
+  mount(AppLayout, { content: <div>Welcome to Queens Events!</div> })
+}, {
+  name: 'home'
+})
---
+++ imports/ui/layouts/app_layout.jsx
@@ -0,0 +1,4 @@
+
+import React from 'react'
+
+export const AppLayout = ({content}) => <div>{content}</div>
